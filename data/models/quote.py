5cae98ed8c438a22453ead76b8c5655d99df6080
hax <vlasovalexs@ya.ru>
2020-10-31 18:32:14 +0300

    quote rating migration

--- data/models/quote.py
+++ data/models/quote.py
@@ -4,3 +4,4 @@ from django.db import models
 class Quote(models.Model):
     text = models.TextField(null=True)
     book = models.ForeignKey('data.Catalog', on_delete=models.DO_NOTHING)
+    rating = models.IntegerField(null=True)
